5b32af2281f732dd8412a41bb6abaa1203625d36
Norbert Mielczarek <nri1234.nm@gmail.com>
2019-09-01 10:59:35 +0200

    Add navigation menu

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
+import { Router, hashHistory } from "react-router";
 import store from "./store/index";
+import routes from "./routes";
 import DevTools from "./DevTools";
+import styles from "./country.css";
 import { getCountries } from "./actions/actions-countries";
 
 render(
     <Provider store={store}>
-        <div>
-            <h1>Inicjalizacja projektu</h1>
-            <DevTools />
-        </div>
+    <div>
+    <Router history={hashHistory} routes={routes} />
+    <DevTools />
+    </div>
     </Provider>,
     document.getElementById("root")
 );
